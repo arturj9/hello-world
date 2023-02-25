a024ca4b59eacf5c079063ffd6b332d9d8bebbc4
arturj9 <arturjardel9@gmail.com>
2023-02-25 16:40:48 -0300

    [add] rust

---
+++ rust/target/debug/deps/rust-1abf79eb8ca45c8f.d
@@ -0,0 +1,5 @@
+/home/artur/projetos/pessoais/hello-world/rust/target/debug/deps/rust-1abf79eb8ca45c8f: src/main.rs
+
+/home/artur/projetos/pessoais/hello-world/rust/target/debug/deps/rust-1abf79eb8ca45c8f.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+/home/artur/projetos/pessoais/hello-world/rust/target/debug/rust: /home/artur/projetos/pessoais/hello-world/rust/src/main.rs
